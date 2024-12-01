ed28c64243c10f5d65521ec023f6186a8579ed93
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2024-12-01 23:07:57 +0300

    - getting broadcast messages from a server

--- Botticelli.Server.Back/Services/Broadcasting/BroadcastService.cs
+++ Botticelli.Server.Back/Services/Broadcasting/BroadcastService.cs
@@ -7,20 +7,16 @@ namespace Botticelli.Server.Back.Services.Broadcasting;
 /// <summary>
 ///     This class is intended for broadcasting
 /// </summary>
-public class BroadcastService : IBroadcastService
+public class BroadcastService(ServerDataContext context) : IBroadcastService
 {
-    private readonly ServerDataContext _context;
-
-    public BroadcastService(ServerDataContext context) => _context = context;
-
     public async Task BroadcastMessage(Broadcast message)
     {
-        _context.BroadcastMessages.Add(message);
-        await _context.SaveChangesAsync();
+        context.BroadcastMessages.Add(message);
+        await context.SaveChangesAsync();
     }
 
     public async Task<IEnumerable<Broadcast>> GetMessages(string botId) 
-        => await _context.BroadcastMessages.Where(m => m.BotId.Equals(botId)).ToArrayAsync();
+        => await context.BroadcastMessages.Where(m => m.BotId.Equals(botId)).ToArrayAsync();
 
-    public async Task DeleteReceived(string botId) => _context.BroadcastMessages.RemoveRange(await GetMessages(botId));
+    public async Task DeleteReceived(string botId) => context.BroadcastMessages.RemoveRange(await GetMessages(botId));
 }
