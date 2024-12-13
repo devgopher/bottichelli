fa7f9f5c462ee911fb4db4234703f48cc26f3324
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2024-12-13 19:08:26 +0300

    - ChainStateKeeper as a workaround for starting/ending an input chain

--- Botticelli.Framework/Commands/Processors/WaitForClientResponseCommandChainProcessor.cs
+++ Botticelli.Framework/Commands/Processors/WaitForClientResponseCommandChainProcessor.cs
@@ -1,4 +1,3 @@
-using System.Collections.Concurrent;
 using Botticelli.Client.Analytics;
 using Botticelli.Framework.Commands.Validators;
 using Botticelli.Interfaces;
@@ -7,20 +6,6 @@ using FluentValidation;
 using Microsoft.Extensions.Logging;
 
 namespace Botticelli.Framework.Commands.Processors;
-
-public static class ChainStateKeeper
-{
-    private static readonly ConcurrentDictionary<string, bool> IsChainOpened = new();
-
-    public static bool GetState(string chatId) => IsChainOpened.TryGetValue(chatId, out var isChainOpened) && isChainOpened;
-    
-    public static void SetState(string chatId, bool isChainOpened) => IsChainOpened[chatId] = isChainOpened;
-
-    public static void SetState(IEnumerable<string> chatIds, bool isChainOpened)
-    {
-        foreach (var chatId in chatIds) SetState(chatId, isChainOpened);
-    }
-}
 
 /// <summary>
 ///     A command with waiting for a response
