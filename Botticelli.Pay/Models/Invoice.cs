f802e2cd3a86a4089f7a3946bfec79495aa7f394
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2025-01-12 12:33:27 +0300

    - currencies

---
+++ Botticelli.Pay/Models/Invoice.cs
@@ -0,0 +1,32 @@
+namespace Botticelli.Pay.Models;
+
+/// <summary>
+///     An invoice entity
+/// </summary>
+public class Invoice
+{
+    /// <summary>
+    ///     Product title
+    /// </summary>
+    public required string Title { get; set; }
+
+    /// <summary>
+    ///     Description
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    ///     Additional parameters for invoice
+    /// </summary>
+    public IEnumerable<string>? AdditionalParameters { get; set; }
+
+    /// <summary>
+    ///     Currency
+    /// </summary>
+    public required string Currency { get; set; }
+
+    /// <summary>
+    ///     Total amount in format: 11.50
+    /// </summary>
+    public required decimal TotalAmount { get; set; }
+}
